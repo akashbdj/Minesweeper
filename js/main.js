090e86a2f06e09bd23c51cc6c7ee25bf6ba8a621
Akash Bhardwaj <akashbdj@gmail.com>
2015-01-31 12:40:14 +0530

    game options

---
+++ js/main.js
@@ -0,0 +1,7 @@
+(function() {
+	document.getElementById('game-options').addEventListener('click', function(){
+		var menu = document.getElementById('options');
+		menu.style.display = menu.style.display === "inline-block" ? "none" : "inline-block";
+
+	});
+}());
