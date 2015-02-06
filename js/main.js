dc5996f091a29d172ff5ebea8fc2833368302475
Akash Bhardwaj <akashbdj@gmail.com>
2015-02-06 13:45:35 +0530

    Bug fix in mines generation

--- js/main.js
+++ js/main.js
@@ -101,20 +101,10 @@
 		var rows = MS.getConfig().rows;
 		var cols = MS.getConfig().cols;
 		var randomNos = [];
+		var uniqueNos = [];
 		var tilesWithMines = [];
 		var firstTile = true;
 		var time;
-
-		function generateUniqueRandomNos(){
-			for(var i=1; i<= mines; i++){
-				var random = Math.floor((Math.random()* (rows*cols))+1);
-				randomNos.push(random);
-			}
-			var uniqueNos = randomNos.filter(function(no, index){
-				return randomNos.indexOf(no) === index;
-			});
-			return uniqueNos;
-		}
 
 		function handleClickedTd(){
 			board.addEventListener("click", function(e) {
@@ -142,13 +132,30 @@
 		}
 
 		function placeMines(tileID){
-			var uniqRnos = generateUniqueRandomNos();
-			for(var i=0; i< uniqRnos.length; i++){
-				if(uniqRnos[i] !== tileID){
-					tilesWithMines.push(uniqRnos[i]);
+			var tilesWithMines = randomPlacesForMines(tileID);
+			console.log("These are the places where mines are placed :=> " + tilesWithMines);
+		}
+
+		function randomPlacesForMines(tileID){
+			for(var i=1; i<= mines; i++){
+				var random = Math.floor((Math.random()* (rows*cols))+1);
+				randomNos.push(random);
+				var lengthBeforeCheck = tilesWithMines.length;
+				tilesWithMines = uniqueAndNotFirstTile(random, i, tileID);
+				var	lengthAfterCheck = tilesWithMines.length;
+				if(lengthBeforeCheck >= lengthAfterCheck){
+					console.log("Either Already present or Same index as of first Clicked tile! Not added!");
+					i--;
 				}
 			}
-			console.log(tilesWithMines);
+			return tilesWithMines;
+		}
+
+		function uniqueAndNotFirstTile(random, i, tileID) {
+			uniqueNos = randomNos.filter(function(no, index){
+				return (randomNos.indexOf(no) === index && random !== tileID);
+			});
+			return uniqueNos;
 		}
 
 		function clickedMine(tileID){
