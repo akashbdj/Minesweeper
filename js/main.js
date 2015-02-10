3e0129c7f610a304ff9c7f2a26dab71034528be4
Akash Bhardwaj <akashbdj@gmail.com>
2015-02-10 06:13:46 +0530

    re-implementation using mvc pattern

--- js/main.js
+++ js/main.js
@@ -1,197 +1,138 @@
-(function() {
-	var MS = (function(){
-		var config = {
-			rows: 9,
-			cols: 9,
-			mines: 10
-		};
+(function(){
 
-		function getGameConfig(){
-			return config;
-		}
+  var Game = (function(){
 
-		function setGameConfig(rows, cols, mines){
-			config.rows = rows;
-			config.cols =  cols;
-			config.mines = mines;
-		}
+    var model = {
 
-		return {
-			setConfig: setGameConfig,
-			getConfig: getGameConfig
-		};
+      config: {
+        rows: 9,
+        cols: 9,
+        mines: 10,
 
-	})();
+        setConfig: function(rows,cols, mines){
+          this.rows = rows;
+          this.cols = cols;
+          this.mines = mines;
+        }
+      },
 
-	var initialize = (function(){
+      tile: {
+        isOpened: false,
+        isMine: false,
+        isEmpty: false,
+        isFlagged: false,
+        isFirstTile: true
+      },
 
-		function toggleOptions(){
-			var menu = document.getElementById('js-options');
-			menu.style.display = menu.style.display === "inline-block" ? "none" : "inline-block";
-		}
+      status: {
+        isGameOver: false,
+        isTimeOut: false
+      },
 
-		function handleToggleEvent(){
-			document.getElementById('game-options').addEventListener('click', function(){
-				toggleOptions();
-			});
-		}
+      elements: {
+        clock: document.getElementById('js-clock'),
+        smiley: document.getElementById('js-play-mood'),
+        menu: document.getElementById('js-options'),
+        board: document.getElementById('js-game-board'),
+        noOfMines: document.getElementById('js-no-of-mines'),
+        gameOptions: document.getElementById('game-options')
+      }
+    };
 
-		function selectDifficulty(){
-			var beginner = document.getElementById('beginner');
-			var intermediate = document.getElementById('intermediate');
-			var expert = document.getElementById('expert');
-			if(beginner.checked){
-				MS.setConfig(9,9,10);
-			}
-			else if(intermediate.checked){
-				MS.setConfig(16,16,40);
-			}
-			else if(expert.checked){
-				MS.setConfig(16,30,99);
-			}
-			console.log(MS.getConfig());
-			generateBoard();
-		}
+    var views = {
 
-		function newGameInit(){
-			var newGame = document.getElementById('js-new-game');
-			newGame.addEventListener('click', function(){
-				selectDifficulty();
-				toggleOptions();
-			});
-		}
+      menuToggle: function(){
+        model.elements.menu.style.display = model.elements.menu.style.display === "inline-block" ? "none" : "inline-block";
+      },
 
-		function generateBoard(){
-			var board = document.getElementById('js-game-board');
-			var noOfMines = document.getElementById('js-no-of-mines');
+      generateBoard: function(){
+        var rows = model.config.rows;
+        var cols = model.config.cols;
+        var mines = model.config.mines;
+        var html= "",td, id =1;
 
-			var rows = MS.getConfig().rows;
-			var cols = MS.getConfig().cols;
-			var mines = MS.getConfig().mines;
-			var html = "", td, id=1;
+        for(var i=0; i<rows; i++){
+          html += '<tr>';
+          for(var j=0; j<cols; j++){
+            td = '<td class="tile" id="{id}"></td>';
+            html += td.replace("{id}", id);
+            id++;
+          }
+          html += '</tr>';
+        }
+        model.elements.board.innerHTML = html;
+        model.elements.noOfMines.innerHTML = mines;
+      },
 
-			for(var i=0; i<rows; i++){
-				html += '<tr>';
-				for(var j=0; j<cols; j++){
-					td = '<td class="tile" id="{id}"></td>';
-					html += td.replace("{id}", id);
-					id++;
-				}
-				html += "</tr>";
-			}
-			board.innerHTML = html;
-			noOfMines.innerHTML = mines;
-		}
+      setSmiley: function(mood){
 
-		return function(){
-			handleToggleEvent();
-			newGameInit();
-			generateBoard();
-		};
+      }
 
-	})();
+    };
 
-	initialize();
+    var controller = {
 
-	var startGame = (function(){
-		var emoticons = document.getElementById('js-play-mood');
-		var clock = document.getElementById('js-clock');
-		var board = document.getElementById("js-game-board");
-		var mines = MS.getConfig().mines;
-		var rows = MS.getConfig().rows;
-		var cols = MS.getConfig().cols;
-		var randomNos = [];
-		var uniqueNos = [];
-		var tilesWithMines = [];
-		var firstTile = true;
-		var time;
+      handleMenuToggle: function(){
+        model.elements.gameOptions.addEventListener('click', function(){
+          views.menuToggle();
+        });
+      },
 
-		function handleClickedTd(){
-			board.addEventListener("click", function(e) {
-				if(e.target && e.target.nodeName === "TD") {
-					tileID = parseInt(e.target.id, 10);
-					tile = document.getElementById(e.target.id);
-					if(firstTile === true){
-						play(tileID);
-						tile.classList.add('opened-tile');
-						firstTile = false;
-					}
-					else if(clickedMine(tileID)){
-						lost();
-					}
-					else {
-						tile.classList.add('opened-tile');
-					}
-				}
-			});
-		}
+      selectDifficulty: function(){
+        var beginner = document.getElementById('beginner');
+        var intermediate = document.getElementById('intermediate');
+        var expert = document.getElementById('expert');
+        if(beginner.checked){
+          model.config.setConfig(9,9,10);
+        }
+        else if(intermediate.checked){
+          model.config.setConfig(16,16,40);
+        }
+        else if(expert.checked){
+          model.config.setConfig(16,30,99);
+        }
+      },
 
-		function play(tileID){
-			timer();
-			placeMines(tileID);
-		}
+      newGameInit: function(){
+        var newGame = document.getElementById('js-new-game');
+        newGame.addEventListener('click', function(){
+          controller.selectDifficulty(); // On newGame - Select difficulty level and generate the game board.
+          views.generateBoard(); // generates boards with the chosen difficulty.
+          views.menuToggle(); // hide Menu after the new Game has been initialized.
+        });
+      },
 
-		function placeMines(tileID){
-			var tilesWithMines = randomPlacesForMines(tileID);
-			console.log("These are the places where mines are placed :=> " + tilesWithMines);
-		}
+      handleMouseEvents: function(){
+        model.elements.board.addEventListener('click', function(e){
+          if(e.target && e.target.nodeName === 'TD'){
+            var tileId = parseInt(e.target.id, 10);
+            var tile = document.getElementById(tileId);
 
-		function randomPlacesForMines(tileID){
-			for(var i=1; i<= mines; i++){
-				var random = Math.floor((Math.random()* (rows*cols))+1);
-				randomNos.push(random);
-				var lengthBeforeCheck = tilesWithMines.length;
-				tilesWithMines = uniqueAndNotFirstTile(random, i, tileID);
-				var	lengthAfterCheck = tilesWithMines.length;
-				if(lengthBeforeCheck >= lengthAfterCheck){
-					console.log("Either Already present or Same index as of first Clicked tile! Not added!");
-					i--;
-				}
-			}
-			return tilesWithMines;
-		}
+            if(model.tile.isFirstTile === true){
+              controller.playGame(tileId);
+              model.tile.isFirstTile = false;
+            }
+          }
+        });
+      },
 
-		function uniqueAndNotFirstTile(random, i, tileID) {
-			uniqueNos = randomNos.filter(function(no, index){
-				return (randomNos.indexOf(no) === index && random !== tileID);
-			});
-			return uniqueNos;
-		}
+      playGame: function(tileId){
 
-		function clickedMine(tileID){
-			for(var k=0; k< tilesWithMines.length; k++){
-				if(tileID === tilesWithMines[k]){
-					return true;
-				}
-			}
-		}
+      }
 
-		function lost(){
-			clearInterval(time);
-			for(var i =0; i< tilesWithMines.length; i++){
-				tileWithMine = document.getElementById(tilesWithMines[i]);
-				tileWithMine.classList.add('mines');
-			}
-		}
+    };
 
-		function timer(){
-			var counter = 0;
-			time = setInterval(function(){
-				if(counter < 999){
-					counter++;
-					clock.innerHTML = counter;
-				}
-				else {
-					clearInterval(time);
-					emoticons.setAttribute("src", "images/sad.png");
-				}
-			}, 1000);
-		}
+    return {
+      init: function(){
+        views.generateBoard(); // generates board with default config (9,9,10)
+        controller.handleMenuToggle(); // handles hide/show mechanism of Menu
+        controller.newGameInit(); // generates boards if the player happens to choose difficulty and start a new game.
+        controller.handleMouseEvents();
+      }
+    };
 
-		return {
-			findClickedTd: handleClickedTd
-		};
+  })();
 
-	})();
-	startGame.findClickedTd();
+  Game.init();
+
 }());
